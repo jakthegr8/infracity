657880eef19478205ff7be307f9792f0b305e69d
Bragadeesh J <bragboy@gmail.com>
2016-12-18 05:47:16 +0530

    Send the average for the Road info

--- app/controllers/roads_controller.rb
+++ app/controllers/roads_controller.rb
@@ -1,6 +1,6 @@
 class RoadsController < ApplicationController
   def info
-    road = Road.find(params[:id])
+    road = Road.includes(:ratings).find_by(id: params[:id])
 
     render json: {
       id: road.id, 
@@ -10,7 +10,18 @@ class RoadsController < ApplicationController
         ward_member: "J.P. Chandrika"
       },
       photos: road.users_roads_photo_maps.map{|urpm| request.base_url+urpm.photo.url}.uniq,
-      complaints: ["669GBM"]
+      complaints: ["669GBM"],
+      encroachment: average(road.ratings.collect(&:encroachment)),
+      safety: average(road.ratings.collect(&:safety)),
+      platform_usability: average(road.ratings.collect(&:platform_usability)),
+      road_quality: average(road.ratings.collect(&:road_quality))
     }
+  end
+
+  private
+
+  def average(list)
+    return -1 if list.size == 0
+    list.sum / list.size
   end
 end
